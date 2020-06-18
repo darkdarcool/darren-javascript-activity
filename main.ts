3b28b1d0bfd6144ded4258262240efe93d4cd81c
darkdarcool <66882633+darkdarcool@users.noreply.github.com>
2020-06-18 14:37:43 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,46 @@
-// This function Prints out your name and favorite color
-function nameAndColor(name:string, color:string){
+function multiplyByFive (startingNum: number) {
+    result = startingNum * 5
+    console.log(result)
+}
+function nameGradeSchool (name: string, Grade: number, school: string) {
+    console.log("Hello " + name + ". Your grade is " + Grade + " at the school, " + school)
+}
+// This function Prints out your name and favorite
+// color
+function nameAndColor (name: string, color: string) {
     console.log("Hello " + name + ". Your favorite color is " + color)
-} 
-
-nameAndColor("Darren", "Teal")   
-
-
-
-// This function Prints out your name and favorite number
-function nameAndNumber(name:string, favNum:number){
+}
+// This function Prints out your name and favorite
+// number
+function nameAndNumber (name: string, favNum: number) {
     console.log("Hello " + name + ". Your favorite number is " + favNum)
 }
+function multiplyByTwo (startingNum: number) {
+    result = startingNum * 2
+    console.log(result)
+}
+let result = 0
+nameAndColor("Darren", "Teal")
+nameAndNumber("Darren", 6969)
+nameGradeSchool("Darren", 7, "Moreland Middle School")
+multiplyByTwo(3)
+multiplyByFive(4)
 
-nameAndNumber("Darren",6969)
+
+function divideByTwo(startingNum: number) {
+    result = startingNum / 2
+    console.log(result)
+}
+
+divideByTwo(50)
+
+function dvideAndSubtract(startingNum: number) {
+   result = startingNum /2-20 
+   console.log(result)
+}
+dvideAndSubtract(50)
+
+
 
 
 
